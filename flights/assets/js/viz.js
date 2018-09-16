4ac427ee91431aec0b3e1e44174414bf7d688868
chomsky <leonhardat@gmail.com>
2018-09-16 21:53:56 +0200

    Added data to the javascript file

--- flights/assets/js/viz.js
+++ flights/assets/js/viz.js
@@ -25,13 +25,10 @@ var 	formatAsPercentage = d3.format("%"),
 function dsPieChart(){
 
 	var dataset = [
-			{category: "Sam", measure: 0.30},
-	      {category: "Peter", measure: 0.25},
-	      {category: "John", measure: 0.15},
-	      {category: "Rick", measure: 0.05},
-	      {category: "Lenny", measure: 0.18},
-	      {category: "Paul", measure:0.04},
-	      {category: "Steve", measure: 0.03}
+			{ category: "B" , measure: 0.399493575097865},
+            { category: "A" , measure: 0.395317024899224},
+            { category: "C" , measure: 0.205102085364611},
+            { category: "D" , measure: 8.73146382991108e-05}
 	      ]
 	      ;
 
@@ -108,7 +105,7 @@ function dsPieChart(){
 		vis.append("svg:text")
 	     	.attr("dy", ".35em")
 	      .attr("text-anchor", "middle")
-	      .text("Revenue Share 2012")
+	      .text("Cancellation Codes 2008")
 	      .attr("class","title")
 	      ;
 
@@ -151,47 +148,39 @@ dsPieChart();
 
 
 
-var datasetBarChart = [
-{ group: "All", category: "Oranges", measure: 63850.4963 },
-{ group: "All", category: "Apples", measure: 78258.0845 },
-{ group: "All", category: "Grapes", measure: 60610.2355 },
-{ group: "All", category: "Figs", measure: 30493.1686 },
-{ group: "All", category: "Mangos", measure: 56097.0151 },
-{ group: "Sam", category: "Oranges", measure: 19441.5648 },
-{ group: "Sam", category: "Apples", measure: 25922.0864 },
-{ group: "Sam", category: "Grapes", measure: 9720.7824 },
-{ group: "Sam", category: "Figs", measure: 6480.5216 },
-{ group: "Sam", category: "Mangos", measure: 19441.5648 },
-{ group: "Peter", category: "Oranges", measure: 22913.2728 },
-{ group: "Peter", category: "Apples", measure: 7637.7576 },
-{ group: "Peter", category: "Grapes", measure: 23549.7526 },
-{ group: "Peter", category: "Figs", measure: 1909.4394 },
-{ group: "Peter", category: "Mangos", measure: 7637.7576 },
-{ group: "John", category: "Oranges", measure: 1041.5124 },
-{ group: "John", category: "Apples", measure: 2430.1956 },
-{ group: "John", category: "Grapes", measure: 15275.5152 },
-{ group: "John", category: "Figs", measure: 4166.0496 },
-{ group: "John", category: "Mangos", measure: 11803.8072 },
-{ group: "Rick", category: "Oranges", measure: 7406.3104 },
-{ group: "Rick", category: "Apples", measure: 2545.9192 },
-{ group: "Rick", category: "Grapes", measure: 1620.1304 },
-{ group: "Rick", category: "Figs", measure: 8563.5464 },
-{ group: "Rick", category: "Mangos", measure: 3008.8136 },
-{ group: "Lenny", category: "Oranges", measure: 7637.7576 },
-{ group: "Lenny", category: "Apples", measure: 35411.4216 },
-{ group: "Lenny", category: "Grapes", measure: 8332.0992 },
-{ group: "Lenny", category: "Figs", measure: 6249.0744 },
-{ group: "Lenny", category: "Mangos", measure: 11803.8072 },
-{ group: "Paul", category: "Oranges", measure: 3182.399 },
-{ group: "Paul", category: "Apples", measure: 867.927 },
-{ group: "Paul", category: "Grapes", measure: 1808.18125 },
-{ group: "Paul", category: "Figs", measure: 795.59975 },
-{ group: "Paul", category: "Mangos", measure: 578.618 },
-{ group: "Steve", category: "Oranges", measure: 2227.6793 },
-{ group: "Steve", category: "Apples", measure: 3442.7771 },
-{ group: "Steve", category: "Grapes", measure: 303.77445 },
-{ group: "Steve", category: "Figs", measure: 2328.93745 },
-{ group: "Steve", category: "Mangos", measure: 1822.6467 },
+var datasetBarChart = [{ group: "A" , category: "1" , measure: 8198},
+                       { group: "All" , category: "1" , measure: 20513},
+                       { group: "B" , category: "1" , measure: 7254},
+                       { group: "C" , category: "1" , measure: 5061},
+                       { group: "A" , category: "2" , measure: 8622},
+                       { group: "All" , category: "2" , measure: 23168},
+                       { group: "B" , category: "2" , measure: 9445},
+                       { group: "C" , category: "2" , measure: 5099},
+                       { group: "D" , category: "2" , measure: 2},
+                       { group: "A" , category: "3" , measure: 9696},
+                       { group: "All" , category: "3" , measure: 20202},
+                       { group: "B" , category: "3" , measure: 6972},
+                       { group: "C" , category: "3" , measure: 3530},
+                       { group: "D" , category: "3" , measure: 4},
+                       { group: "A" , category: "4" , measure: 8584},
+                       { group: "All" , category: "4" , measure: 17884},
+                       { group: "B" , category: "4" , measure: 5895},
+                       { group: "C" , category: "4" , measure: 3403},
+                       { group: "D" , category: "4" , measure: 2},
+                       { group: "A" , category: "5" , measure: 7680},
+                       { group: "All" , category: "5" , measure: 23962},
+                       { group: "B" , category: "5" , measure: 10909},
+                       { group: "C" , category: "5" , measure: 5372},
+                       { group: "D" , category: "5" , measure: 1},
+                       { group: "A" , category: "6" , measure: 5277},
+                       { group: "All" , category: "6" , measure: 13567},
+                       { group: "B" , category: "6" , measure: 6700},
+                       { group: "C" , category: "6" , measure: 1587},
+                       { group: "D" , category: "6" , measure: 3},
+                       { group: "A" , category: "7" , measure: 6273},
+                       { group: "All" , category: "7" , measure: 18138},
+                       { group: "B" , category: "7" , measure: 7729},
+                       { group: "C" , category: "7" , measure: 4136}
 ]
 ;
 
@@ -345,7 +334,7 @@ function dsBarChart() {
 		.attr("y", 15)
 		.attr("class","title")
 		.attr("text-anchor", "middle")
-		.text("Overall Sales Breakdown 2012")
+		.text("Cancellations by Day Of Week")
 		;
 }
 
@@ -426,7 +415,7 @@ function updateBarChart(group, colorChosen) {
 			.attr("y", 15)
 			.attr("class","title")
 			.attr("text-anchor", "middle")
-			.text(group + "'s Sales Breakdown 2012")
+			.text(group + "'s Cancellations by Day Of Week")
 		;
 }
 
@@ -436,47 +425,61 @@ function updateBarChart(group, colorChosen) {
 -------------------------------------------
 */
 
-var datasetLineChart = [
-{ group: "All", category: 2008, measure: 289309 },
-{ group: "All", category: 2009, measure: 234998 },
-{ group: "All", category: 2010, measure: 310900 },
-{ group: "All", category: 2011, measure: 223900 },
-{ group: "All", category: 2012, measure: 234500 },
-{ group: "Sam", category: 2008, measure: 81006.52 },
-{ group: "Sam", category: 2009, measure: 70499.4 },
-{ group: "Sam", category: 2010, measure: 96379 },
-{ group: "Sam", category: 2011, measure: 64931 },
-{ group: "Sam", category: 2012, measure: 70350 },
-{ group: "Peter", category: 2008, measure: 63647.98 },
-{ group: "Peter", category: 2009, measure: 61099.48 },
-{ group: "Peter", category: 2010, measure: 87052 },
-{ group: "Peter", category: 2011, measure: 58214 },
-{ group: "Peter", category: 2012, measure: 58625 },
-{ group: "Rick", category: 2008, measure: 23144.72 },
-{ group: "Rick", category: 2009, measure: 14099.88 },
-{ group: "Rick", category: 2010, measure: 15545 },
-{ group: "Rick", category: 2011, measure: 11195 },
-{ group: "Rick", category: 2012, measure: 11725 },
-{ group: "John", category: 2008, measure: 34717.08 },
-{ group: "John", category: 2009, measure: 30549.74 },
-{ group: "John", category: 2010, measure: 34199 },
-{ group: "John", category: 2011, measure: 33585 },
-{ group: "John", category: 2012, measure: 35175 },
-{ group: "Lenny", category: 2008, measure: 69434.16 },
-{ group: "Lenny", category: 2009, measure: 46999.6 },
-{ group: "Lenny", category: 2010, measure: 62180 },
-{ group: "Lenny", category: 2011, measure: 40302 },
-{ group: "Lenny", category: 2012, measure: 42210 },
-{ group: "Paul", category: 2008, measure: 7232.725 },
-{ group: "Paul", category: 2009, measure: 4699.96 },
-{ group: "Paul", category: 2010, measure: 6218 },
-{ group: "Paul", category: 2011, measure: 8956 },
-{ group: "Paul", category: 2012, measure: 9380 },
-{ group: "Steve", category: 2008, measure: 10125.815 },
-{ group: "Steve", category: 2009, measure: 7049.94 },
-{ group: "Steve", category: 2010, measure: 9327 },
-{ group: "Steve", category: 2011, measure: 6717 },
-{ group: "Steve", category: 2012, measure: 7035 }
+var datasetLineChart = [{ group: "A" , category: "1" , measure: 6635},
+                        { group: "All" , category: "1" , measure: 17308},
+                        { group: "B" , category: "1" , measure: 6711},
+                        { group: "C" , category: "1" , measure: 3962},
+                        { group: "A" , category: "2" , measure: 6090},
+                        { group: "All" , category: "2" , measure: 20596},
+                        { group: "B" , category: "2" , measure: 10034},
+                        { group: "C" , category: "2" , measure: 4470},
+                        { group: "D" , category: "2" , measure: 2},
+                        { group: "A" , category: "3" , measure: 6038},
+                        { group: "All" , category: "3" , measure: 16183},
+                        { group: "B" , category: "3" , measure: 7520},
+                        { group: "C" , category: "3" , measure: 2621},
+                        { group: "D" , category: "3" , measure: 4},
+                        { group: "A" , category: "4" , measure: 7312},
+                        { group: "All" , category: "4" , measure: 10355},
+                        { group: "B" , category: "4" , measure: 1479},
+                        { group: "C" , category: "4" , measure: 1564},
+                        { group: "A" , category: "5" , measure: 3157},
+                        { group: "All" , category: "5" , measure: 6229},
+                        { group: "B" , category: "5" , measure: 1461},
+                        { group: "C" , category: "5" , measure: 1608},
+                        { group: "D" , category: "5" , measure: 3},
+                        { group: "A" , category: "6" , measure: 4251},
+                        { group: "All" , category: "6" , measure: 10931},
+                        { group: "B" , category: "6" , measure: 3448},
+                        { group: "C" , category: "6" , measure: 3232},
+                        { group: "A" , category: "7" , measure: 5292},
+                        { group: "All" , category: "7" , measure: 10598},
+                        { group: "B" , category: "7" , measure: 2612},
+                        { group: "C" , category: "7" , measure: 2694},
+                        { group: "A" , category: "8" , measure: 3852},
+                        { group: "All" , category: "8" , measure: 9835},
+                        { group: "B" , category: "8" , measure: 3582},
+                        { group: "C" , category: "8" , measure: 2401},
+                        { group: "A" , category: "9" , measure: 2246},
+                        { group: "All" , category: "9" , measure: 9913},
+                        { group: "B" , category: "9" , measure: 6478},
+                        { group: "C" , category: "9" , measure: 1188},
+                        { group: "D" , category: "9" , measure: 1},
+                        { group: "A" , category: "10" , measure: 2097},
+                        { group: "All" , category: "10" , measure: 3249},
+                        { group: "B" , category: "10" , measure: 600},
+                        { group: "C" , category: "10" , measure: 552},
+                        { group: "A" , category: "11" , measure: 2510},
+                        { group: "All" , category: "11" , measure: 4458},
+                        { group: "B" , category: "11" , measure: 937},
+                        { group: "C" , category: "11" , measure: 1009},
+                        { group: "D" , category: "11" , measure: 2},
+                        { group: "A" , category: "12" , measure: 4850},
+                        { group: "All" , category: "12" , measure: 17779},
+                        { group: "B" , category: "12" , measure: 10042},
+                        { group: "C" , category: "12" , measure: 2887}
+
+
 ]
 ;
 
@@ -582,7 +585,7 @@ function dsLineChart() {
 	    ;
 
 	svg.append("text")
-		.text("Performance 2012")
+		.text("Monthly Cancellations 2018")
 		.attr("id","lineChartTitle1")
 		.attr("x",margin.left + ((width + margin.right)/2))
 		.attr("y", 10)
